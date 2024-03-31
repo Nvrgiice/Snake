d25eb6a9113a1789e9044fe9f5676fc3dfe96036
Nvrgiice <133342311+Nvrgiice@users.noreply.github.com>
2024-03-31 16:31:55 +0200

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,4 @@
+from .afficher_acceuil import afficher_acceuil # . du répertoire courant " a partir d'ici "
+from .ouvrir_regles import ouvrir_regles
+
+__all__ = ['afficher_acceuil', 'ouvrir_regles'] # defini tout les fichiers importés quand on importe all dis *
