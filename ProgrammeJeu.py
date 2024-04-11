9c4f419f7f1dbc6f9282cb336b15695431f0f89a
Clovis3 <144457194+Clovis3@users.noreply.github.com>
2024-04-11 09:48:03 +0200

    Update ProgrammeJeu.py

--- ProgrammeJeu.py
+++ ProgrammeJeu.py
@@ -1,5 +1,5 @@
 from tkinter import *
-from random import randint
+from random import *
 import math 
 
 
@@ -13,15 +13,92 @@ fenetre.config(background='#288352')
 
 
 #variable selon taille de écran
-PGCD = math.gcd(fenetre.winfo_screenwidth() , fenetre.winfo_screenheight())   
+PGCD = math.gcd(w , h)   
 C = int(PGCD/3)    #côté de carreau
-reste_longeur = fenetre.winfo_screenwidth()%C
-colonne = int(fenetre.winfo_screenwidth()//C)
-ligne = int((fenetre.winfo_screenheight()-5*C)//C)
+reste_longeur = w%C
+colonne = int(w//C)
+ligne = int((h-5*C)//C)
 
 # Créaction Caneva
-can = Canvas(fenetre,bg='yellow',height=C*ligne ,width=fenetre.winfo_screenwidth())
+can = Canvas(fenetre,bg='yellow',height=C*ligne ,width=C*colonne)
 can.pack(side=BOTTOM)
+
+
+#=========================================================================================
+
+ecb = int(C//8) #espace cercle bord
+
+def mouvement():
+    global Serpent
+    i=len(Serpent)-1
+
+    can.delete("corps")
+    can.delete("tete")
+
+    while i > 0:
+        Serpent[i][0]=Serpent[i-1][0]    #transmet l'abscisse au cercle avant
+        Serpent[i][1]=Serpent[i-1][1]    #transmet l'aordonnée au cercle avant
+        can.create_oval(Serpent[i][0]-3*ecb, Serpent[i][1]-3*ecb, Serpent[i][0]+3*ecb, Serpent[i][1]+3*ecb,outline='green', fill='black',tags = 'corps') 
+        i -= 1
+
+    if direction  == 'gauche':
+        Serpent[0][0]  = Serpent[0][0] - dx
+        if Serpent[0][0] < 0:
+            Serpent[0][0] = w-C//2
+    elif direction  == 'droite':
+        Serpent[0][0]  = Serpent[0][0] + dx
+        if Serpent[0][0] > w:
+            Serpent[0][0] = C//2
+    elif direction  == 'haut':
+        Serpent[0][1]  = Serpent[0][1] - dy
+        if Serpent[0][1] < 0:
+            Serpent[0][1] = C*ligne-C//2
+    elif direction  == 'bas':
+        Serpent[0][1]  = Serpent[0][1] + dy
+        if Serpent[0][1] > C*ligne:
+            Serpent[0][1] = C//2
+    
+    can.create_oval(Serpent[i][0]-3*ecb, Serpent[i][1]-3*ecb, Serpent[i][0]+3*ecb, Serpent[i][1]+3*ecb,outline='green', fill='blue', tags='tete')
+
+    test_pomme()
+    test_perdu()
+
+
+    if flag != 0:
+        fenetre.after(150,mouvement)   # temps 
+
+    #return distance and point
+
+
+def gauche(event):
+    global direction
+    direction = 'gauche'
+ 
+def droite(event):
+    global direction
+    direction = 'droite'
+ 
+def haut(event):
+    global direction
+    direction = 'haut'
+ 
+def bas(event):
+    global direction
+    direction = 'bas'
+
+
+def test_pomme():
+    global pomme
+    global x,y,x_pomme,y_pomme
+    global Serpent
+    if Serpent[1][0]>x_pomme-C//2 and  Serpent[1][0]<x_pomme+C//2:        
+        if Serpent[1][1]>y_pomme-C//2 and Serpent[1][1]<y_pomme+C//2:
+            soutient(x_pomme+1-C//2,y_pomme)
+            images = []
+            x_pomme, y_pomme = pomme()
+            Serpent.append([0,0]) #On joute un nouveau point au serpent
+
+    #return point
 
 
 def creation_terrain():
@@ -47,20 +124,18 @@ def creation_terrain():
         x_fin += C
 
 
-
-#génération de pomme au hazard
-def pomme():
+def pomme():                                   #génération de pomme au hazard
     ligne_rand = randint(1, ligne)
     colone_rand = randint(1, colonne)
+    # est-ce que la case ligne_rand,colone_rand appartient au snake ?
+    # si oui
+    # relancer pomme()
+    # sinon code suivant
     monImage = PhotoImage(file="pomme.png").subsample(10)
     images.append(monImage)  # Garder une référence à l'image
     can.create_image(colone_rand * C - C//2, ligne_rand * C - C//2, image=monImage)
 
-    
     return colone_rand * C - C//32, ligne_rand * C - C//2
-
-images = []
-x_pomme, y_pomme = pomme()
 
 
 def soutient(x,y):
@@ -83,95 +158,38 @@ def soutient(x,y):
             #claire
 
 
-#=========================================================================================
-
-ecb = int(C//8) #espace cercle bord
-
-def mouvement():
+def test_perdu():
+    global x,y
     global Serpent
-    i=len(Serpent)-1
-    j=0
-    while i > 0:
-        Serpent[i][0]=Serpent[i-1][0]    #transmet l'abscisse au cercle avant
-        Serpent[i][1]=Serpent[i-1][1]    #transmet l'aordonnée au cercle avant
-        can.create_oval(Serpent[i][0]-3*ecb, Serpent[i][1]-3*ecb, Serpent[i][0]+3*ecb, Serpent[i][1]+3*ecb,outline='green', fill='black',) 
-        i -= 1
+    t = len(Serpent)-1
+    while t > 0:
+        if Serpent[0][0]>Serpent[t][0]-C//2 and  Serpent[0][0]<Serpent[t][0]+C//2:        
+            if Serpent[0][1]>Serpent[t][1]-C//2 and Serpent[0][1]<Serpent[t][1]+C//2:
+                #can.delete('all')
+                popup_perdu()
+        t-=1
 
-    if direction  == 'gauche':
-        Serpent[0][0]  = Serpent[0][0] - dx
-        if Serpent[0][0] < 0:
-            Serpent[0][0] = fenetre.winfo_screenwidth()-C//2
-    elif direction  == 'droite':
-        Serpent[0][0]  = Serpent[0][0] + dx
-        if Serpent[0][0] > fenetre.winfo_screenwidth():
-            Serpent[0][0] = C//2
-    elif direction  == 'haut':
-        Serpent[0][1]  = Serpent[0][1] - dy
-        if Serpent[0][1] < 0:
-            Serpent[0][1] = C*ligne-C//2
-    elif direction  == 'bas':
-        Serpent[0][1]  = Serpent[0][1] + dy
-        if Serpent[0][1] > C*ligne:
-            Serpent[0][1] = C//2
-    can.create_oval(Serpent[i][0]-3*ecb, Serpent[i][1]-3*ecb, Serpent[i][0]+3*ecb, Serpent[i][1]+3*ecb,outline='green', fill='blue')
+def popup_perdu() :
+    fen_perdu = Toplevel()
+    fen_perdu.iconbitmap("snake.ico")
+    fen_perdu.config(background='#1d7677')
+    #fen_perdu.title('Erreur')
+    fen_perdu.geometry("400x200+400+400") # +400 + 400 : permet de positionner le popup sur l'écran - en partant du coin en haut à gauche
+    message = Label(fen_perdu, text="Perdu !!", fg="#6ef31d", bg="#1d7677", font='Calibri 30 bold')
+    message.pack()
+
+    cadre = Frame(fen_perdu)
+
+    btAjouter = Button(cadre, text = "Rejouer", width=10, height=1)
+    btVider = Button(cadre, text = "Retour vers menu", width=15, height=1,)
+    btQuitter = Button(cadre, text = "Quitter", width=10, height=1, command=fenetre.destroy)
+
+    btAjouter.pack(side=LEFT)
+    btQuitter.pack(side=RIGHT)
+    btVider.pack()
     
-    soutient(Serpent[i-1][0],Serpent[i-1][1]) 
-    test_pomme()
-    #test_que(i)
-
-
-    if flag != 0:
-        fenetre.after(150, mouvement)   # temps 
-
-
-
-    #return distance and point
-
-    # torp lourd
-    #monImage = PhotoImage(file="pomme.png").subsample(10)
-    #images.append(monImage)
-    #can.create_image(1+x_pomme - C//2, y_pomme, image=monImage)
-    #================
-
-def gauche(event):
-    global direction
-    direction = 'gauche'
- 
-def droite(event):
-    global direction
-    direction = 'droite'
- 
-def haut(event):
-    global direction
-    direction = 'haut'
- 
-def bas(event):
-    global direction
-    direction = 'bas'
+    cadre.pack(expand=YES)
     
-def test_pomme():
-    global pomme
-    global x,y,x_pomme,y_pomme
-    global Serpent
-    if Serpent[1][0]>x_pomme-C//2 and  Serpent[1][0]<x_pomme+C//2:        
-        if Serpent[1][1]>y_pomme-C//2 and Serpent[1][1]<y_pomme+C//2:
-            soutient(x_pomme+1-C//2,y_pomme)
-            images = []
-            x_pomme, y_pomme = pomme()
-            Serpent.append([0,0]) #On joute un nouveau point au serpent
-
-    #return point
-
-
-
-#def test_que(i):
-#    a = len(Serpent)-1
-#    while a > 0 :
-#        if Serpent[i][0] == Serpent[a][0] and Serpent[i][1] == Serpent[a][1]:
-#            can.delete("all")    
-#    a-=1
-
-
 
 
 flag = 1
@@ -183,19 +201,33 @@ fenetre.bind('<q>', gauche)
 fenetre.bind('<z>' , haut)
 fenetre.bind('<s>', bas)
 
-x=3*C + C//2
-y=3*C + C//2
-Serpent=[[x,y],[x,y],[x,y],[x,y]]
+x=6*C + C//2
+y=6*C + C//2
+Serpent=[[x,y],[x,y-C],[x,y-2*C]]
 
+
+
+#=========================================================================================
+#sous_titre = Label(fenetre, text = "Nouvel élément", font="Calibri, 20", 
+#fg='pink', bg='#3396ff')
+# Affichage du sous titre à une position définie
+#sous_titre.place(x=100, y=100)
+
+#=========================================================================================
 
 def play():
     creation_terrain()
     global images
     global x_pomme, y_pomme
+    images = []
     x_pomme, y_pomme = pomme()
     mouvement()
 
 play()
 
+
+
 fenetre.mainloop()
 #pygame
+
+#deiconify et withdraw
