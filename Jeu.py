e0b5c0ac2b9f36af9f3744b3198e77369631df9a
Clovis3 <144457194+Clovis3@users.noreply.github.com>
2024-04-19 11:46:06 +0200

    Update Jeu.py

--- Jeu.py
+++ Jeu.py
@@ -5,11 +5,11 @@ import time
 
 
 def initialisation():
+    global resultat_mouvement, distance_a, score_a, fenetre, w, h, point2, distance2, temps2, C, colonne, ligne, can, ecb, flag, dx, dy, direction, x, y, Serpent, temps_début
 
-    global distance, score, fenetre, w, h, point2, distance2, temps2, C, colonne, ligne, can, ecb, flag, dx, dy, direction, x, y, Serpent, temps_début
-
-    distance = 0  # initialisation de la distance
-    score = 0     # initialisation du score (nombre de point)
+    resultat_mouvement = None  # Variable globale pour stocker le résultat de mouvement()
+    distance_a = 0             # initialisation de la distance
+    score_a = 0                # initialisation du score (nombre de point)
 
     # Création d’une "fenêtre"
     fenetre = Tk()
@@ -23,7 +23,6 @@ def initialisation():
     cadre1 = Frame(cadreO, bg='#3396ff', bd=5, relief=SUNKEN)          # cadre pour les points
     cadre2 = Frame(cadreO, bg='#3396ff', bd=5, relief=SUNKEN)          # cadre pour la distance
     cadre3 = Frame(cadreO, bg='#3396ff', bd=5, relief=SUNKEN)          # cadre pour le temps
-
 
     point1 = Label(cadre1, text = "Point(s) : ", font="Calibri, 15", fg='pink', bg='#3396ff')       # écriture du cadre 1
     point1.pack()                                                                                   # écriture en haut du cadre 1
@@ -39,7 +38,6 @@ def initialisation():
     temps1.pack()                                                                                   # écriture en haut du cadre 2
     temps2 = Label(cadre3, text = f"{0}min ; {0}s", font="Calibri, 10", fg='pink', bg='#3396ff')    # initialisation de la valeur de temps
     temps2.pack()                                                                                   # position en bas du cadre 3
-
 
     #variable selon taille de écran
     PGCD = math.gcd(w , h)     #détermination du PGCD en fonction de la longueur et de la largeur
@@ -70,11 +68,8 @@ def initialisation():
 
 #======================================programme principale====================================================
 
-
 def mouvement():
-    global Serpent
-    global distance, score, temps
-    global flag
+    global resultat_mouvement, Serpent, distance_a, score_a, temps_a, flag
 
     can.delete("corps")      # efface le corps précédent
     can.delete("tete")       # efface la tête précédente
@@ -110,34 +105,25 @@ def mouvement():
     
     can.create_oval(Serpent[i][0]-3*ecb, Serpent[i][1]-3*ecb, Serpent[i][0]+3*ecb, Serpent[i][1]+3*ecb,outline='green', fill='blue', tags='tete')          # créer la tête du serpend
 
-    score = test_pomme()
+    score_a = test_pomme()
     test_pomme()   
     flag = test_perdu()
     
     temps_encours = time.time()
 
-    distance += 1                     #incrémentatin du compteur de distance à chaque mouvement
-    #temps = round(distance*0.150,3)   #calcule la 
+    distance_a += 1                     #incrémentatin du compteur de distance à chaque mouvement
 
-    temps = round(temps_encours-temps_début)
+    temps_a = round(temps_encours-temps_début)
 
-    
     apparition_donnée()
 
+    if flag != 0:                                                         # si condition d'erreur non vérifier     
+        fenetre.after(150, mouvement)   
+    else:                                                                 # si condition d'erreur vérifier
+        temps_a = f"{int(temps_a//60)}min;{int(temps_a%60)}s"             # transformer de manière lisible
+        resultat_mouvement = (score_a,temps_a, distance_a) 
 
-    if flag != 0:                                                #si condition d'erreur non vérifier
-        fenetre.after(150,mouvement)   # temps 
-        #return distance, score
-
-    else :                                                       #si condition d'erreur vérifier
-        print(f"dans mouvement {distance} {score}")
-        return distance, score
-
-
-
-    
-
-#========================================programme appeller dans le programme principale==========================
+#========================================fonction qui appelle tout==============================
 
 def gauche(event):           # fonction qui détermine la direction = gauche si appeler
     global direction
@@ -155,70 +141,60 @@ def bas(event):              # fonction qui détermine la direction = bas si app
     global direction
     direction = 'bas'
 
+def creation_terrain():                                                                        # fonction qui créer le terrain
 
-def creation_terrain():      # fonction qui créer le terrain
-
-    #les carreau serons de longueur et de largeur C (PGCD divisé par 3)
+    # les carreau serons de longueur et de largeur C (PGCD divisé par 3)
     x_début = 0
     y_début = 0
     x_fin = C
     y_fin = C
 
-    for i in range (0,colonne):  
+    for i in range (0,colonne):                                                                # le terrain sera fait de colone à colone
         t = i                                                                                  # la variable t permet d'alterner les deux couleur possible de carreau
-        for j in range (0,ligne) :
+        for j in range (0,ligne) :                                                             # les colones serons fait carreau par careau
             if t%2 == 1 :
                 couleur = '#32c258'                                                            # vert foncée (adapté au terrain)
             else :
                 couleur = 'light green'                                                        # vert claire
             t+=1                                                                               # permet d'alternation grâce à l'incrémentation
             carrée = can.create_rectangle(x_début,y_début,x_fin,y_fin,width=1,fill=couleur)    # créer les carreau
-            y_début += C                                                                       #
-            y_fin += C
+            y_début += C                                                                       # déplace le haut de la création d'un carreau vers le bas
+            y_fin += C                                                                         # déplace le bas de la création d'un carreau vers le bas
         
-        y_début = 0
-        y_fin = C
-        x_début += C
-        x_fin += C
+        y_début = 0                                                                            # réhinitialise y_début pour redescendre par la boucle
+        y_fin = C                                                                              # réhinitialise y_fin pour redescendre par la boucle
+        x_début += C                                                                           # déplace le coté gaucher de la création de carreau vers la droite
+        x_fin += C                                                                             # déplace le coté droit de la création de carreau vers la droite
 
-def pomme():                                   #génération de pomme au hazard
-    ligne_rand = randint(1, ligne)
-    colone_rand = randint(1, colonne)
-    # est-ce que la case ligne_rand,colone_rand appartient au snake ?
-    # si oui
-    # relancer pomme()
-    # sinon code suivant
-    monImage = PhotoImage(file="pomme.png").subsample(10)
-    images.append(monImage)  # Garder une référence à l'image
-    can.create_image(colone_rand * C - C//2, ligne_rand * C - C//2, image=monImage)
+def pomme():                                                                           # génération de pomme au hazard
+    ligne_rand = randint(1, ligne)                                                     # choisi au hazard la ligne d'apparition de la pomme 
+    colone_rand = randint(1, colonne)                                                  # choisi au hazard la colone d'apparition de la pomme 
+    monImage = PhotoImage(file="pomme.png").subsample(10)                              # variable représentant l'image de la pomme à une taille déterminer
+    images.append(monImage)                                                            # Garder une référence à l'image
+    can.create_image(colone_rand * C - C//2, ligne_rand * C - C//2, image=monImage)    # place la pomme en fonction de la lingne et de la colone choisi précédement
 
     return colone_rand * C - C//32, ligne_rand * C - C//2
 
-def soutient(x,y):
-    m = int(x//C)
-    o = int(y//C)
+def soutient(x,y):                                                                                          # fonction qui repose un carreau de la bonne couleur en fonction de ces entrée de position x et y  (indispensable pour effacer la pomme)
+    m = int(x//C)                                                                                           # détermine la ligne du carreau à repposer
+    o = int(y//C)                                                                                           # détermine la colone du carreau à repposer
     if m%2 == 0 :
-        if o%2 == 0 : 
-            carrée = can.create_rectangle(x-C//2,y-C//2,x+C//2,y+C//2,width=1,fill='light green')
-            #claire
-        else :
-            carrée = can.create_rectangle(x-C//2,y-C//2,x+C//2,y+C//2,width=1,fill='#32c258')
-            #foncée
+        if o%2 == 0 :                                                                                       # carreau de colone paire et de linge paire est vert claire
+            carrée = can.create_rectangle(x-C//2,y-C//2,x+C//2,y+C//2,width=1,fill='light green')           # créer le carreau correspondant au bonne endroit
+        else :                                                                                              # carreau de colone paire et de linge imparie paire est vert fonsé
+            carrée = can.create_rectangle(x-C//2,y-C//2,x+C//2,y+C//2,width=1,fill='#32c258')               # créer le carreau correspondant au bonne endroit
     
     else :
-        if o%2 == 0 : 
-            carrée = can.create_rectangle(x-C//2,y-C//2,x+C//2,y+C//2,width=1,fill='#32c258')
-            #foncée
-        else :
-            carrée = can.create_rectangle(x-C//2,y-C//2,x+C//2,y+C//2,width=1,fill='light green')
-            #claire
-
+        if o%2 == 0 :                                                                                       # carreau de colone impaire et de linge paire est vert fonsé
+            carrée = can.create_rectangle(x-C//2,y-C//2,x+C//2,y+C//2,width=1,fill='#32c258')               # créer le carreau correspondant au bonne endroit
+        else :                                                                                              # carreau de colone impaire et de linge imparie paire est vert claire
+            carrée = can.create_rectangle(x-C//2,y-C//2,x+C//2,y+C//2,width=1,fill='light green')           # créer le carreau correspondant au bonne endroit
 
 def test_pomme():
     global pomme
     global x,y,x_pomme,y_pomme
     global Serpent
-    global score
+    global score_a
 
     if Serpent[1][0]>x_pomme-C//2 and  Serpent[1][0]<x_pomme+C//2:        
         if Serpent[1][1]>y_pomme-C//2 and Serpent[1][1]<y_pomme+C//2:
@@ -226,14 +202,12 @@ def test_pomme():
             images = []
             x_pomme, y_pomme = pomme()
             Serpent.append([0,0])                                           #On joute un nouveau point au serpent
-            score += 1
+            score_a += 1
 
-    return score
-
+    return score_a
 
 def test_perdu():
-    global x,y
-    global Serpent, flag
+    global x, y, Serpent, flag
     t = len(Serpent)-1
     while t > 0:
         if Serpent[0][0]>Serpent[t][0]-C//2 and  Serpent[0][0]<Serpent[t][0]+C//2:        
@@ -265,21 +239,18 @@ def popup_perdu() :
 
     cadre.pack(expand=YES)
 
-
 def apparition_donnée():
-    global distance , score, temps
-    point2.configure(text=score)
-    distance2.configure(text=distance)
-    temps2.configure(text=f"{int(temps//60)}min ; {int(temps%60)}s")
-
-
-#=============================caractère permmetant de contrôler la direction=======================================
+    global distance_a , score_a, temps_a
+    point2.configure(text=score_a)
+    distance2.configure(text=distance_a)
+    temps2.configure(text=f"{int(temps_a//60)}min ; {int(temps_a%60)}s")
 
 def dir() :
     fenetre.bind('<d>', droite)     # si touche d appuyer appelle la fonction droite
     fenetre.bind('<q>', gauche)     # si touche q appuyer appelle la fonction gauche
     fenetre.bind('<z>' , haut)      # si touche z appuyer appelle la fonction haut
     fenetre.bind('<s>', bas)        # si touche s appuyer appelle la fonction bas
+
 
 #========================================fonction qui appelle tout=================================================
 
@@ -288,22 +259,22 @@ def play():
     temps_début = time.time()
     global images
     global x_pomme, y_pomme
-    global distance , score
+    global distance_a , score_a
 
     initialisation()
     creation_terrain()
     images = []
     x_pomme, y_pomme = pomme()
-    mouvement()                                         # La fonction continue alors que mouvement encore actif
+    mouvement()                                          # La fonction continue alors que mouvement encore actif
     dir()
-    print(time.time())
-    print(" distance: ", distance, "score", score)
-    
+    fenetre.mainloop()
 
+    score_b = resultat_mouvement[0]
+    temps_b = resultat_mouvement[1]
+    distance_b = resultat_mouvement[2]
 
+    return score_b, temps_b, distance_b
 
-play()
+score_b, temps_b, distance_b = play()
 
-
-
-fenetre.mainloop()
+print(score_b, temps_b, distance_b)
